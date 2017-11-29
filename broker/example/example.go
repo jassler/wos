f30894a10c0cee7d2af73a7449a63dad14c07928
BuildTools <unconfigured@null.spigotmc.org>
2017-11-30 00:34:52 +0100

    Setup go structure

---
+++ broker/example/example.go
@@ -0,0 +1,6 @@
+package example
+
+// GenerateExample returns example string
+func GenerateExample() string {
+	return "We need 20 windows. How much do you charge?"
+}
---
+++ broker/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "broker/example"
+import "fmt"
+
+func main() {
+	fmt.Println(example.GenerateExample())
+}
